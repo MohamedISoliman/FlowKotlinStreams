704f344a7c173616d94b88e724454ac4cdacfdbc
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-02-19 01:49:09 +0300

    sopes and Dispatchers

--- src/main/kotlin/CourotineIntro.kt
+++ src/main/kotlin/CourotineIntro.kt
@@ -1,29 +1,54 @@
 import kotlinx.coroutines.*
 
 fun main() {
-    GlobalScope.launch {
-        val millisCoroutine = measureTimeMillisCoroutine { funA() }
-        println("Took $millisCoroutine")
-    }
-    Thread.sleep(2300)
-    println("Exiting main...")
+    WelcomingScreen().onCreate()
+    Thread.sleep(2000)
 }
 
-suspend fun funA() {
+suspend fun funA(): String {
     println("start....")
-
     val helloDeffered = GlobalScope.async { funB() }
     val worldDeffered = GlobalScope.async { funC() }
-    println("${helloDeffered.await()} ${worldDeffered.await()}")
     println("......end")
+    return helloDeffered.await() + " " + worldDeffered.await()
 }
 
 suspend fun funB(): String {
+    println("funB Thread: ${Thread.currentThread()}")
     delay(1000)
     return "Hello"
 }
 
 suspend fun funC(): String {
+    println("funC Thread: ${Thread.currentThread()}")
     delay(1000)
     return "World!"
+}
+
+
+class WelcomingScreen : Activity, CoroutineScope by CoroutineScope(Dispatchers.Default) {
+
+    override fun onCreate() {
+        launch(Dispatchers.IO) {
+            println("Launch Thread: ${Thread.currentThread()}")
+            val string = funA()
+            withContext(Dispatchers.Default) {
+                println("Logging Thread: ${Thread.currentThread()}")
+                println(string)
+            }
+
+        }
+    }
+
+    override fun onDestroy() {
+    }
+
+}
+
+//fake base activity
+interface Activity {
+
+    fun onCreate()
+
+    fun onDestroy()
 }
